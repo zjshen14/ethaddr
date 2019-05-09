85212591109d6fe1bca56c7111a8db3e36b4b8be
Zhijie Shen <zjshen14@gmail.com>
2019-05-08 22:14:09 -0700

    Fix the bug

--- prettyaddr.go
+++ prettyaddr.go
@@ -9,36 +9,30 @@ import (
 	"github.com/ethereum/go-ethereum/crypto"
 )
 
-var numZero int
+var numZeros int
+var numThreads int
 
 func init()  {
-	flag.IntVar(&numZero, "num-zero", 0, "Number of zero bytes at the end of address")
+	flag.IntVar(&numZeros, "num-zeros", 1, "Number of zero bytes at the end of address")
+	flag.IntVar(&numThreads, "num-threads", 4, "Number of threads to mine")
 }
 func main() {
-	chs := make(chan interface{})
 	done := make(chan interface{})
-	defer close(chs)
-	for {
-		select {
-		case <-done:
-			return
-		case chs<-nil:
-		default:
-			go genKey(numZero, chs, done)
-		}
-
+	for i := 0; i < numThreads; i++ {
+		go genKey(numZeros, done)
 	}
+	<-done
 }
 
-func genKey(numZero int, chs <-chan interface{}, done chan<- interface{}) {
-	bs := make([]byte, numZero)
-	for range chs {
+func genKey(numZeros int, done chan<- interface{}) {
+	bs := make([]byte, numZeros)
+	for {
 		sk, err := crypto.GenerateKey()
 		if err != nil {
 			log.Panic(err.Error())
 		}
 		addr := crypto.PubkeyToAddress(sk.PublicKey)
-		if bytes.Equal(addr.Bytes()[20-numZero:], bs) {
+		if bytes.Equal(addr.Bytes()[20-numZeros:], bs) {
 			fmt.Printf("Private Key: %x\n", crypto.FromECDSA(sk))
 			fmt.Printf("Public Key: %x\n", crypto.FromECDSAPub(&sk.PublicKey))
 			fmt.Printf("ETH Address: %s\n", addr.Hex())
